9dde2adf34dbccf1c28d31822d197bef12d7be96
Anwesh43 <anweshthecool0@gmail.com>
2020-08-16 16:35:19 +0530

    addding DrawingUtil class

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
+import { sienna } from "color-name"
+
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const parts : number = 3 
-const scGap : number = 0.02 
+const lines : number = 4 
+const scGap : number = 0.02 / parts 
 const strokeFactor : number = 90 
 const sizeFactor : number = 4.2 
 const rFactor : number = 3.8 
 const delay : number = 20 
 const colors : Array<string> = ["#F44336", "#3F51B5", "#4CAF50", "#2196F3", "#FFEB3B"]
 const backColor : string = "#bdbdbd"
+const rot : number = Math.PI / 4 
 
 class ScaleUtil {
 
@@ -21,6 +25,54 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+    
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawAxisCircleLine(context : CanvasRenderingContext2D, i : number, sf1 : number, sf2 : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const r : number = size / rFactor 
+        context.save()
+        context.rotate(i * Math.PI / 2)
+        DrawingUtil.drawCircle(context, 0, -size, r * sf1)
+        DrawingUtil.drawLine(context, 0, -size, size * sf2, -size + size * sf2)
+        context.restore()
+    }
+
+    static drawAxisCirclesRot(context : CanvasRenderingContext2D, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(sf3 * rot)
+        for (var j = 0; j < lines; j++) {
+            DrawingUtil.drawAxisCircleLine(context, j, sf1, sf2)
+        }
+        context.restore()
+    }
+
+    static drawACRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawAxisCirclesRot(context, scale)
     }
 }
 
