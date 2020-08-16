c2d187cb8e802747881badc81fd6e5468435b6d3
Anwesh43 <anweshthecool0@gmail.com>
2020-08-16 16:45:44 +0530

    adding AxisCircleRot classs

--- index.ts
+++ index.ts
@@ -192,3 +192,26 @@ class ACRNode {
         return this 
     }
 }
+
+class AxisCircleRot {
+
+    curr : ACRNode = new ACRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
