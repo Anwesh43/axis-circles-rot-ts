4a1b7e9e43d72c663e22c76d9501342cd86d7bf8
Anwesh43 <anweshthecool0@gmail.com>
2020-08-16 16:44:00 +0530

    adding ACRNode class

--- index.ts
+++ index.ts
@@ -150,3 +150,45 @@ class Animator {
         }
     }
 }
+
+class ACRNode {
+
+    prev : ACRNode 
+    next : ACRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ACRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawACRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : ACRNode {
+        var curr : ACRNode = this.prev
+        if (dir == 1) {
+            curr = this.next 
+        } 
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
